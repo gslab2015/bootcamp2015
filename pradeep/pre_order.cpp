ad340dfa9b13f5ef67e68ff35483890e185b6e81
unknown <gs-0677@GS-2236.GSLAB.COM>
2015-07-06 17:07:21 +0530

    ROOM-0001 : it's not in burhan's release branch

--- pradeep/pre_order.cpp
+++ pradeep/pre_order.cpp
@@ -4,7 +4,8 @@ using namespace std;
 
 int main(int args, char ** argv)
 {
-	cout << "Pre-order .." << endl ;
+	cout << "Pre-order .. it's not in burhar's release brance" << endl ;
 
 	return 0;
+
 }
